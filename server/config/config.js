1f6844c0436d67bd382618fde93a743ef78637d4
amIsmael <maischingon2@msn.com>
2020-06-12 19:02:13 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:KDR7HrAJswPuB8u@cluster0-yhc4c.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
